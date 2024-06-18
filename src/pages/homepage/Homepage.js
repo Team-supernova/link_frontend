8508566ec8cd556c1f77c31d6f9d4f945b6ce2c7
Nwosa <obere4u@gmail.com>
2024-06-18 21:16:47 +0100

    Removed unsed code

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,11 +1,12 @@
-import Navbar from "../../components/Navbar/index.js";
 import PageLayout from "../../components/PageLayout/index.js";
-import Hero from "./components/Hero.js"
+import Hero from "./components/Hero.js";
 
 export default function Homepage() {
-  return <PageLayout>
-    <div>
-      <Hero />
-    </div>
-  </PageLayout>;
+  return (
+    <PageLayout>
+      <div>
+        <Hero />
+      </div>
+    </PageLayout>
+  );
 }
